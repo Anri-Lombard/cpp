f1afb943effe7aa3cac7afece1fc8d925c8b0140
Anri Lombard <anri.m.lombard@gmail.com>
2021-07-24 10:21:24 +0200

    confused

---
+++ codeforces/div3/1538D_APADN.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using cd = complex<ld>;
+
+int main() {
+    int a, b, k;
+    cin >> a >> b >> k;
+
+    
+}
