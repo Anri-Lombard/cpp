5990ed02afced91ba6bfd8f9106669fc3616250c
Anri Lombard <anri.m.lombard@gmail.com>
2021-07-31 10:59:00 +0200

    cpp progress

---
+++ codeforces/800 - 1200/bad_boy.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define nl "\n"
+
+
+void solve() {
+
+    int n, m, i, j;
+    cin >> n >> m >> i >> j;
+
+    cout << 1 << " " << 1 << " " << n << " " << m << nl;
+}
+
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+}
--- codeforces/800 - 1200/love_song.cpp
+++ codeforces/800 - 1200/love_song.cpp
@@ -14,5 +14,16 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
+    int n, q, l, r, total; cin >> n >> q;
+    vector<char> song;
+    for (int i =0; i<n;i++) {
+        char let; cin >> let;
+        song[i] = let;
+    }
+    while (q--) {
+        total = 0;
+        cin >> l >> r;
+        
+        cout << total << nl;
+    }
 }
