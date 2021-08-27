cb82b1d3c1adbb58697a029e0e1bdf5b5b6c6b78
Anri Lombard <anri.m.lombard@gmail.com>
2021-08-27 06:49:54 +0200

    Progress

--- USACOGuide/Bronze/simulation/speeding.cpp
+++ USACOGuide/Bronze/simulation/speeding.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
+#include <vector>
 #include <cstdio>
+#include <iostream>
 
 int max_over_speed = 0;
 int road_length = 100;
@@ -9,11 +11,33 @@ int main() {
     scanf("%d%d", &N, &M);
     int road_lengths[N], speed_limits[N], driven_lengths[M], driven_speeds[M];
 
+    int start = 0;
+    std::vector<int> limit(road_length);
     for (int i=0; i<N; ++i) {
         int ln, lm;
         scanf("%d%d", &ln, &lm);
 
-        road_length[i] = ln;
-        speed_limits[i] = lm;
+        for (int i=0; i<start+ln; i++) {
+            limit[i] = lm;
+        }
+        start += ln;
     }
+
+    start = 0;
+    std::vector<int> bessie(road_length);
+    for (int i=0; i<M; ++i) {
+        int ln, lm;
+        std::cin >> ln >> lm;
+        for (int i=0; i<start+ln; i++) {
+            bessie[i] = lm;
+        }
+        start += ln;
+    }
+
+    int worst = 0;
+    for (int i=0; i<road_length; i++) {
+        worst = std::max(worst, bessie[i] - limit[i]);
+    }
+
+    std::cout << worst << "\n";
 }
