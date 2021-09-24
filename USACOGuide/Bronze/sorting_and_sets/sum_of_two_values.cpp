7a4c94cbebb631fbac688404a95d148573eef6d2
Anri Lombard <anri.m.lombard@gmail.com>
2021-09-24 20:41:59 +0200

    mapps

--- USACOGuide/Bronze/sorting_and_sets/sum_of_two_values.cpp
+++ USACOGuide/Bronze/sorting_and_sets/sum_of_two_values.cpp
@@ -1,1 +1,19 @@
-#include <bits/stdc++.>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x; cin >> n >> x;
+
+    vector<int> a(n);
+    map<int, int> vals;
+    for (int i=0; i<n; i++)
+        cin >> a[i];
+    for (int i=0; i< n; i++) {
+        if (vals.count(x-a[i])) {
+            cout << i + 1 << " " << vals[x - a[i]] << "\n";
+            return 0;
+        }
+        vals[a[i]] = i + 1;
+    }
+    cout << "IMPOSSIBLE" << "\n";
+}
